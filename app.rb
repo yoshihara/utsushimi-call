ad6fc13a971c1770b4d82a61438a6c9953e74eb3
yoshihara <h.yoshihara@everyleaf.com>
2015-08-06 23:03:58 +0900

    Implement almost
    TOOD:
      - Add Gemfile
      - Save record file in other place (S3?)
      - refactor
      - test

---
+++ app.rb
@@ -0,0 +1,53 @@
+require "sinatra/base"
+require "sinatra/reloader"
+require "open-uri"
+require "pry"
+require "slack-poster"
+require 'twilio-ruby'
+
+class UtsushimiCall < Sinatra::Base
+  register Sinatra::Reloader
+
+  SLACK_HOOK_URL = ENV["SLACK_HOOK_URL"].dup.freeze
+
+  get "/" do
+    Twilio::TwiML::Response.new do |r|
+      r.Say "こんにちは", language: "ja-jp"
+      r.Gather numDigits: '1', action: '/records/new', method: 'get' do |g|
+        g.Say "いち、を、おしてください", language: "ja-jp"
+      end
+    end.text
+  end
+
+  get "/records/new" do
+    response = nil
+    if params['Digits'] == '1'
+      response = Twilio::TwiML::Response.new do |r|
+        r.Say "トーン音の、あとに、おはなしください", language: "ja-jp"
+        r.Record maxLength: '5', action: '/records', method: 'post'
+      end
+    else
+      response = Twilio::TwiML::Response.new do |r|
+        r.Say "さようなら", language: "ja-jp"
+      end
+    end
+
+    response.text
+  end
+
+  post "/records" do
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "録音しました", language: "ja-jp"
+      r.Say 'Goodbye.'
+    end.text
+
+    # File.open("voice-#{Time.now.strftime("%Y%m%d_%H%M%S_%N")}.wav", "wb") { |f| f.print(open(params['RecordingUrl']).read) }
+
+    poster = Slack::Poster.new(SLACK_HOOK_URL)
+    poster.send_message "#{params['From']}から電話がありました。\n #{params['RecordingUrl']}"
+
+    response
+  end
+
+  run! if app_file == $0
+end
